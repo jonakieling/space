219d66cdafd6fdf9dd8536e1bf613dc32942a043
Jonathan Kieling <code@j0na.net>
2018-04-21 23:49:43 +0200

    Moving storages internally to the heap. plus clean-up.

--- src/storage.rs
+++ src/storage.rs
@@ -3,23 +3,20 @@ use scene::LEVEL_SIZE;
 
 #[derive(Clone)]
 pub struct PositionLevelStorage<T: Clone> {
-    storage: Vec<Option<Box<T>>>
+    storage: Box<Vec<Option<Box<T>>>>
 }
 
 impl<T: Clone> PositionLevelStorage<T> {
     pub fn new() -> PositionLevelStorage<T> {
         PositionLevelStorage {
-            storage: vec![None; (LEVEL_SIZE * LEVEL_SIZE) as usize]
+            storage: Box::new(vec![None; (LEVEL_SIZE * LEVEL_SIZE) as usize])
         }
     }
     
     pub fn get(&self, x: i32, y: i32) -> Option<&Option<Box<T>>> {
         if x <= LEVEL_SIZE && y <= LEVEL_SIZE  {
-        let position = x + y * LEVEL_SIZE;
-            match self.storage.get(position as usize) {
-                Some(item) => Some(item),
-                None => None
-            }
+            let position = x + y * LEVEL_SIZE;
+            self.storage.get(position as usize)
         } else {
             None
         }
@@ -27,11 +24,8 @@ impl<T: Clone> PositionLevelStorage<T> {
 
     pub fn get_mut(&mut self, x: i32, y: i32) -> Option<&mut Option<Box<T>>> {
         if x <= LEVEL_SIZE && y <= LEVEL_SIZE  {
-        let position = x + y * LEVEL_SIZE;
-            match self.storage.get_mut(position as usize) {
-                Some(item) => Some(item),
-                None => None
-            }
+            let position = x + y * LEVEL_SIZE;
+            self.storage.get_mut(position as usize)
         } else {
             None
         }
