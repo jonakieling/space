baa9c689409a34d08414ef3433497053c4f86242
Jonathan Kieling <code@j0na.net>
2018-05-05 18:59:35 +0200

    Selecting dialog options and exiting a conversation

--- src/input/npc.rs
+++ src/input/npc.rs
@@ -14,6 +14,17 @@ pub fn key_up_event(scene: &mut Scene, _ctx: &mut Context, keycode: Keycode, _ke
     		}
             scene.input = InputState::World;
         },
+        Keycode::Return => {
+        	let mut dialog = scene.dialog.clone().unwrap();
+        	if dialog.children.iter().len() > 0 {
+        		scene.dialog = Some(dialog.children.current().unwrap().clone());	
+        	} else {
+	    		if let Some(npc) = scene.current_npc() {
+	        	    npc.direction = npc.look_at;
+	    		}
+            	scene.input = InputState::World;
+        	}
+        },
         Keycode::Up => {
         	if let Some(ref mut dialog) = scene.dialog {
             	dialog.children.prev();
