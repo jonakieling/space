dfb0f9d706cad1a5aa81087e1867f0b7301104d1
Jonathan Kieling <code@j0na.net>
2018-05-13 17:55:14 +0200

    making static ship and station a bit more rich in content.

--- src/savegame/static_levels.rs
+++ src/savegame/static_levels.rs
@@ -77,26 +77,32 @@ pub fn static_station_outpost(scene_data: &mut SceneData) {
     scene_data.backdrop = String::from("");
 
     super::insert_walls(scene_data, vec![
-        (6, 8), (7, 8), (8, 8), (9, 8), (10, 8),
-        (6, 9),                         (10, 9),
-        (6, 10),                        (10, 10),
-                (7, 11),        (9, 11)
+        (6, 8), (7, 8), (8, 8), (9, 8), (10, 8), (11, 8),
+        (6, 9),                                  (11, 9),
+        (6, 10),                                 (11, 10),
+        (6, 11),                                 (11, 11),
+        (6, 12),                                 (11, 12),(12, 12),(13, 12), (14, 12),
+        (6, 13),                                 (11, 13),                   (14, 13),
+        (6, 14),                                                    
+        (6, 15),                                 (11, 15),                   (14, 15),
+        (6, 16),(7, 16),(8, 16),(9, 16),(10, 16),(11, 16),(12, 16),(13, 16), (14, 16)       
     ]);
 
     super::insert_doors(scene_data, vec![
-        (8, 11, DoorStatus::Closed)
+        (11, 14, DoorStatus::Closed)
     ]);
 
-    super::insert_circuitry(scene_data, vec![
-        (8, 13)
-    ]);
-
-    scene_data.terminals.insert(Position::new(8, 9), Terminal {
+    scene_data.terminals.insert(Position::new(14, 14), Terminal {
         text: Box::new(String::new()),
-        front: Direction::Down
+        front: Direction::Left
     });
 
-    super::insert_player(scene_data, (8, 10), Direction::Up, vec![]);
+    super::insert_storage(scene_data, vec![
+        (7, 9),
+        (7, 10)
+    ]);
+
+    super::insert_player(scene_data, (9, 11), Direction::Down, vec![]);
 
     println!("game loaded: static station outpost");
 }
@@ -108,15 +114,46 @@ pub fn static_ship_tech(scene_data: &mut SceneData) {
         (6, 8), (7, 8), (8, 8), (9, 8), (10, 8),
         (6, 9),                         (10, 9),
         (6, 10),                        (10, 10),
-                (7, 11),        (9, 11)
+                (7, 11),        (9, 11),
+        (6, 12),                        (10, 12),
+        (6, 13),
+        (6, 14),                        (10, 14),
+        (6, 15),                        (10, 15),
+        (6, 16),                        (10, 16),
+        (6, 17),(7, 17),(8, 17),(9, 17),(10, 17)
     ]);
 
     super::insert_doors(scene_data, vec![
-        (8, 11, DoorStatus::Closed)
+        (8, 11, DoorStatus::Open),
+        (10, 13, DoorStatus::Closed)
+    ]);
+
+    super::insert_generator(scene_data, vec![
+        (8, 15)
     ]);
 
     super::insert_circuitry(scene_data, vec![
-        (8, 13)
+                        (10, 2),
+                        (10, 3),          (12, 3),
+                        (10, 4),          (12, 4),
+                        (10, 5),          (12, 5),
+                        (10, 6),          (12, 6),
+                        (10, 7),          (12, 7),
+        (8, 8), (9, 8), (10, 8), (11, 8), (12, 8),
+        (8, 9),         (10, 9),
+                        (10, 10),
+                        (10, 11),
+                        (10, 12),
+                        (10, 13),
+                        (10, 14),
+                (9, 15),(10, 15)
+    ]);
+
+    scene_data.update_power();
+
+    super::insert_storage(scene_data, vec![
+        (7, 12),
+        (7, 13)
     ]);
 
     scene_data.terminals.insert(Position::new(8, 9), Terminal {
@@ -126,5 +163,5 @@ pub fn static_ship_tech(scene_data: &mut SceneData) {
 
     super::insert_player(scene_data, (8, 10), Direction::Up, vec![]);
 
-    println!("game loaded: static ship tech 2.1");
+    println!("game loaded: static ship tech");
 }
