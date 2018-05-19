e2bd5df3f96222a7718091b2a66a478b18745d43
Jonathan Kieling <code@j0na.net>
2018-05-20 00:11:15 +0200

    slowing movement a bit

--- src/constants.rs
+++ src/constants.rs
@@ -2,4 +2,4 @@ pub const LEVEL_SIZE: i32 = 40;
 pub const TILE_SIZE: i32 = 8;
 pub const PIXEL_SCALE: i32 = 5;
 pub const GRID_SIZE: i32 = TILE_SIZE * PIXEL_SCALE;
-pub const MOVEMENT_SPEED: u64 = 220;
+pub const MOVEMENT_SPEED: u64 = 275;
